d0dc1cc83e566251f9547a8031aeaaf4a684ab19
ACES <acesmw.dev@gmail.com>
2025-07-29 10:40:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 const CONFIG = JSON.parse(process.env.CONFIG_JSON);
 const FIREBASE_CONN = JSON.parse(process.env.FIREBASE_CONN_JSON);
-module.exports = CONFIG, FIREBASE_CONN;
+module.exports = {
+  CONFIG,
+  FIREBASE_CONN,
+};
